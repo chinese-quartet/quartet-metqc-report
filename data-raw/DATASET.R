6c865aaa539db11e62a3251d1eea454091fb901e
nxzhang <nxzhang96@gmail.com>
2021-09-17 10:25:36 +0800

    Update metabolites mapping system

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,19 +1,20 @@
 ## code to prepare `DATASET` dataset goes here
 library(data.table)
-sample_data <- sample_data
-cols <- colnames(sample_data)[-1]
-sample_data <- sample_data[sample_data[,..cols][,!Reduce(`&`, lapply(.SD, is.na))]]
-colnames(sample_data)[1] <- "HMDB_id"
+sample_data <- fread("../../02_modifiedData/0916/T_L1.csv")[,1:14]
 
-sample_metadata <- load("~/Projects/Quartet/Data/MetforQCtools/MetQC/data/sample_metadata.rda")
-cols <- c("col_names","sample","rep","recode","longsp")
+sample_metadata <- fread("../../02_modifiedData/0916/Metadata.csv")[OldColName %in% colnames(sample_data),]
+cols <- c("OldColName","strategy","lab","sample","rep","batch")
 sample_metadata <- sample_metadata[,..cols]
+colnames(sample_metadata)[1] <- "col_names"
 
+# write.csv(sample_data,"input/sample_data.csv",row.names = F)
+# write.csv(sample_metadata,"input/sample_metadata.csv",row.names = F)
+
+MetInfo <- unique(fread("../../02_modifiedData/0916/MetaboliteAnnot.csv")[,c(2,3)])
 
 multibatches_rawdata <- fread("../../02_modifiedData/0206/20210625_batch1quant.csv")
 multibatches_metadata <- fread("../../02_modifiedData/0206/20210625_batch1metadata.csv")
-MetReference <- readRDS("../../02_modifiedData/0206/20210504_RefDatasets_CVmethod_9.rds")
-colnames(MetReference)[2] <-"HMDB_id"
+MetReference <- readRDS("../../02_modifiedData/0916/20210916_RefDatasets_CVmethod_9.rds")
 HistoricalData <- fread("../../02_modifiedData/0206/20210628_qcmetrics.csv")
 
 # write.csv(sample_data,"input/sample_data.csv",row.names = F)
@@ -22,7 +23,8 @@ HistoricalData <- fread("../../02_modifiedData/0206/20210628_qcmetrics.csv")
 
 usethis::use_data(sample_data,compress = "xz",overwrite = "T")
 usethis::use_data(sample_metadata,compress = "xz",overwrite = "T")
-usethis::use_data(multibatches_rawdata,compress = "xz")
-usethis::use_data(multibatches_metadata,compress = "xz")
-usethis::use_data(MetReference,compress = "xz")
-usethis::use_data(HistoricalData,compress = "xz")
+# usethis::use_data(multibatches_rawdata,compress = "xz",overwrite = "T")
+# usethis::use_data(multibatches_metadata,compress = "xz",overwrite = "T")
+usethis::use_data(MetReference,compress = "xz",overwrite = "T")
+usethis::use_data(MetInfo,compress = "xz",overwrite = "T")
+usethis::use_data(HistoricalData,compress = "xz",overwrite = "T")
